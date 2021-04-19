fea16f67aa5b87e0e236e9f2bf123d5c60b86010
sheff-slava <sheff-slava@mail.ru>
2021-04-19 13:13:39 +0300

    Stage 5

--- cconverter.py
+++ cconverter.py
@@ -1,8 +1,12 @@
-amount = float(input())
+import requests
 
-xchange_rates = [2.98, 0.82, 0.17, 1.9622, 0.208]
-print(f'I will get {round(amount * xchange_rates[0], 2)} RUB from the sale of {amount} conicoins.')
-print(f'I will get {round(amount * xchange_rates[1], 2)} ARS from the sale of {amount} conicoins.')
-print(f'I will get {round(amount * xchange_rates[2], 2)} HNL from the sale of {amount} conicoins.')
-print(f'I will get {round(amount * xchange_rates[3], 2)} AUD from the sale of {amount} conicoins.')
-print(f'I will get {round(amount * xchange_rates[4], 2)} MAD from the sale of {amount} conicoins.')
+
+currency = input().lower()
+
+url = f'http://www.floatrates.com/daily/{currency}.json'
+response = requests.get(url)
+
+json_obj = response.json()
+print(json_obj['usd'])
+print(json_obj['eur'])
+
