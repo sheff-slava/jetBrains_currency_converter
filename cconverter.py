e329536529faf95c5a194b47a28be485c63a6639
sheff-slava <sheff-slava@mail.ru>
2021-04-19 20:39:17 +0300

    Stage 6

--- cconverter.py
+++ cconverter.py
@@ -1,12 +1,28 @@
 import requests
 
 
-currency = input().lower()
+cache = dict()
 
-url = f'http://www.floatrates.com/daily/{currency}.json'
+currency_from = input().lower()
+
+url = f'http://www.floatrates.com/daily/{currency_from}.json'
 response = requests.get(url)
-
 json_obj = response.json()
-print(json_obj['usd'])
-print(json_obj['eur'])
 
+if currency_from != 'usd':
+    cache['usd'] = json_obj['usd']['rate']
+if currency_from != 'eur':
+    cache['eur'] = json_obj['eur']['rate']
+
+while True:
+    currency_to = input().lower()
+    if currency_to == '':
+        break
+    amount = float(input())
+    print('Checking the cache...')
+    if currency_to in cache.keys():
+        print('Oh! It is in the cache!')
+    else:
+        print('Sorry, but it is not in the cache!')
+        cache[currency_to] = json_obj[currency_to]['rate']
+    print(f'You received {round(amount * cache[currency_to], 2)} {currency_to.upper()}.')
