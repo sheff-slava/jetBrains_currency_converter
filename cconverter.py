fdfd4a2c628f561fce84e4c418b80edef758d87d
sheff-slava <sheff-slava@mail.ru>
2021-04-19 12:49:36 +0300

    Stage 3

---
+++ cconverter.py
@@ -0,0 +1,5 @@
+amount = int(input('Please, enter the number of conicoins you have: '))
+xchange_rate = float(input('Please, enter the exchange rate: '))
+print(f'The total amount of dollars: {amount * xchange_rate}')
+
+
