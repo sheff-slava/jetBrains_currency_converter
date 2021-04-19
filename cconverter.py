bebf0c4845efbd1fd1497943b83e5b54f623304f
sheff-slava <sheff-slava@mail.ru>
2021-04-19 12:54:53 +0300

    Stage 4

--- cconverter.py
+++ cconverter.py
@@ -1,5 +1,8 @@
-amount = int(input('Please, enter the number of conicoins you have: '))
-xchange_rate = float(input('Please, enter the exchange rate: '))
-print(f'The total amount of dollars: {amount * xchange_rate}')
+amount = float(input())
 
-
+xchange_rates = [2.98, 0.82, 0.17, 1.9622, 0.208]
+print(f'I will get {round(amount * xchange_rates[0], 2)} RUB from the sale of {amount} conicoins.')
+print(f'I will get {round(amount * xchange_rates[1], 2)} ARS from the sale of {amount} conicoins.')
+print(f'I will get {round(amount * xchange_rates[2], 2)} HNL from the sale of {amount} conicoins.')
+print(f'I will get {round(amount * xchange_rates[3], 2)} AUD from the sale of {amount} conicoins.')
+print(f'I will get {round(amount * xchange_rates[4], 2)} MAD from the sale of {amount} conicoins.')
